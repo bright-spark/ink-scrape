e0ac626f0bdbe80d4f9c5b4ee33478e0e95fb5ef
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 09:38:35 +0000

    show text that is at or after position

--- base/UnmatchedBoundaryException.php
+++ base/UnmatchedBoundaryException.php
@@ -2,7 +2,8 @@
 
 class UnmatchedBoundaryException extends Exception {
   public function __construct($position, $boundary, $text) {
-    parent::__construct("boundary not found in text at or after position {$position}:\n\tboundary: {$boundary}\n\ttext: {$text}\n");
+    $str = substr($text, $position, 15);
+    parent::__construct("boundary not found in text at or after position {$position}:\n\tboundary: {$boundary}\n\ttext (after {$position}): '{$str}'\n");
   }
 }
 
