10dad226fb349bd5d036b8b7a79e7ca0cfe88a80
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 02:59:11 +0000

    reorg into an object not namespace

--- branches/dev/test.php
+++ branches/dev/test.php
@@ -7,22 +7,22 @@ include("ink-scrape.php");
 //Test #1: test ability to locate text (not necessarily in any order).
 $string = " a aga sdg a sgfoo adgamglh ari hahs svbars jgaje jryr eryegaz";
 $pos = 0;
-//$inst =& new InkScrape();
-InkScrape::checkFormatAndMoveReadHead($pos, $string,array(
+$inst =& new InkScrape(0, array(
 'foo',
 'bar',
-'gaz'));
+'gaz'), $string);
+$inst->checkTextFormat();
 echo("passed test\n");
 
 //Test #2: test ability to locate text, in specified order.
 // the third string comes before the second string, so this case should fail if position is stored
 $threw = false;
 try {
-  $pos = 0;
-  InkScrape::checkFormatAndMoveReadHead($pos, $string,array(
+  $inst =& new InkScrape(0, array(
   'foo',
-  'gaz',
-  'bar'));
+  'bar',
+  'gaz'), $string);
+  $inst->checkTextFormat();
 } catch (Exception $e) {
   if($e->getMessage()=="unrecognized page format") {
     $threw = true;
