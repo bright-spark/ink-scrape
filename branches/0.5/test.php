d7fbcc38e956ec5c652bb3ff1aca7be80e61706f
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 10:26:41 +0000

    use javadoc-style

--- branches/0.5/test.php
+++ branches/0.5/test.php
@@ -6,7 +6,9 @@ include("ink-scrape.php");
 
 $string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tortor quam, pellentesque at, blandit id, consequat malesuada, lectus. Etiam laoreet quam vel metus. Suspendisse eu risus id justo tincidunt auctor.";
 
-//Test #1: test ability to locate text (not necessarily in any order).
+/**
+ * Test #1: test ability to locate text (not necessarily in any order).
+ */
 $pos = 0;
 InkScrape::checkTextFormat(0, array(
 'Lorem',
@@ -14,8 +16,10 @@ InkScrape::checkTextFormat(0, array(
 'Etiam'), $string);
 echo("Test #1: passed test\n");
 
-//Test #2: test ability to locate text, in specified order.
-// the third string comes before the second string, so this case should fail if position is stored
+/**
+ * Test #2: test ability to locate text, in specified order.
+ * the third string comes before the second string, so this case should fail if position is stored
+ */
 $threw = false;
 try {
   InkScrape::checkTextFormat(0, array(
@@ -28,7 +32,9 @@ try {
 
 echo("Test #2: ".($threw ? 'passed' : 'failed')." test\n");
 
-//Test #3: test ability to locate text, in specified order, with no overlapping of boundaries.
+/**
+ * Test #3: test ability to locate text, in specified order, with no overlapping of boundaries.
+ */
 $threw = false;
 try {
   $string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tortor quam, pellentesque at, blandit id, consequat malesuada, lectus. Etiam laoreet quam vel metus. Suspendisse eu risus id justo tincidunt auctor.";
