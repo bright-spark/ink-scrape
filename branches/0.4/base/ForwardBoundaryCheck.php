f2b355d7bdbec48715df59caaf35ae76ba829ccf
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 08:21:02 +0000

    add constructor

--- branches/0.4/base/ForwardBoundaryCheck.php
+++ branches/0.4/base/ForwardBoundaryCheck.php
@@ -4,6 +4,12 @@ require_once("IBoundaryChecker.php");
 require_once("SimpleBoundaryChecker.php");
 
 class ForwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryChecker {
+  public function __construct($boundaries=array(/*string*/), $data='', $pos=0) {
+    $this->m_boundaries = $boundaries;
+    $this->m_data = $data;
+    $this->m_pos = $pos;
+  }
+
   public function check() {
     foreach($this->m_boundaries as $boundary) {
       $pos_new = strpos($this->m_data, $boundary, $this->m_pos);
