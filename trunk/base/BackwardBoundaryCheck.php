2452957c2048d8ae8349fbeaa528ed76c0456f9e
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 14:42:08 +0000

    rescue file

---
+++ trunk/base/BackwardBoundaryCheck.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("IBoundaryChecker.php");
+require_once("SimpleBoundaryChecker.php");
+require_once("UnmatchedBoundaryException.php");
+
+class BackwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryChecker {
+  public function __construct($boundaries=array(/*string*/), $data='', $pos=null) {
+    $this->m_boundaries = $boundaries;
+    $this->m_data = $data;
+    $this->m_pos = ($pos==null) ? strlen($data) : $pos;
+  }
+
+  public function check() {
+    $this->m_pos = empty($this->m_pos) ? strlen($this->m_data) : $this->m_pos;
+    $count = 0;
+    foreach($this->m_boundaries as $boundary) {
+      $haystack = substr($this->m_data, 0, $this->m_pos);
+      $pos_new = strrpos($haystack, $boundary);
+      if($pos_new!==false) {
+        $this->m_pos = $pos_new;
+        $count++;
+      } else {
+        throw new UnmatchedBoundaryException($this->m_pos, $boundary, $this->m_data, $count);
+      }
+    }
+  }
+}
+
+?>
