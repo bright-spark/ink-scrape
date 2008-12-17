4b2b86f119e19981e6df027e5888c02495988ef6
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 11:08:36 +0000

    add 'forward' to names to indicate direction, cos we'll be adding direction-specific capability

--- branches/0.5/ink-scrape.php
+++ branches/0.5/ink-scrape.php
@@ -3,8 +3,8 @@
 include("base/ForwardBoundaryCheck.php");
 
 class InkScrape {
-  public static function checkTextFormat($pos, $rules, $text) {
-    $fbc = new ForwardBoundaryCheck($rules, $text, $pos);
+  public static function checkFrontBoundariesForText($pos, $boundaries, $text) {
+    $fbc = new ForwardBoundaryCheck($boundaries, $text, $pos);
     $fbc->check();
   }
 }
