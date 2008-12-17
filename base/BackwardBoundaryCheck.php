387057e8aeeabe9ca1955a7329c014a3a2f17973
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 14:45:30 +0000

    do paramter validation like in ForwardBoundaryCheck

--- base/BackwardBoundaryCheck.php
+++ base/BackwardBoundaryCheck.php
@@ -8,12 +8,17 @@ class BackwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryCh
   public function __construct($boundaries=array(/*string*/), $data='', $pos=null) {
     $this->m_boundaries = $boundaries;
     $this->m_data = $data;
-    $this->m_pos = ($pos==null) ? strlen($data) : $pos;
+    $this->m_pos = $pos;
+  }
+
+  protected function validateParameters() {
+    parent::validateParameters();
+    $this->m_pos = empty($this->m_pos) ? strlen($this->m_data) : $this->m_pos;
   }
 
   public function check() {
-    $this->m_pos = empty($this->m_pos) ? strlen($this->m_data) : $this->m_pos;
     $count = 0;
+    $this->validateParameters();
     foreach($this->m_boundaries as $boundary) {
       $haystack = substr($this->m_data, 0, $this->m_pos);
       $pos_new = strrpos($haystack, $boundary);
