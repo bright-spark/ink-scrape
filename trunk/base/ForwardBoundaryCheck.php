f54079464c026a91acb169d896b3725e125593b8
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 14:09:48 +0000

    *improve test format  *specify boundary checking direction (forwards and backwards)

--- trunk/base/ForwardBoundaryCheck.php
+++ trunk/base/ForwardBoundaryCheck.php
@@ -5,19 +5,22 @@ require_once("SimpleBoundaryChecker.php");
 require_once("UnmatchedBoundaryException.php");
 
 class ForwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryChecker {
-  public function __construct($boundaries=array(/*string*/), $data='', $pos=0) {
+  public function __construct($boundaries, $data, $pos) {
     $this->m_boundaries = $boundaries;
     $this->m_data = $data;
     $this->m_pos = $pos;
   }
 
   public function check() {
+    $count = 0;
+    $this->validateParameters();
     foreach($this->m_boundaries as $boundary) {
       $pos_new = strpos($this->m_data, $boundary, $this->m_pos);
       if($pos_new!==false) {
-        $this->m_pos = $pos_new;
+        $this->m_pos = $pos_new+strlen($boundary);
+        $count++;
       } else {
-        throw new UnmatchedBoundaryException($this->m_pos, $boundary, $this->m_data);
+        throw new UnmatchedBoundaryException($this->m_pos, $boundary, $this->m_data, $count);
       }
     }
   }
