9bcac15576942286c90fa9d49ccddbccf59536ce
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 08:15:21 +0000

    implement checking logic

--- branches/0.4/base/ForwardBoundaryCheck.php
+++ branches/0.4/base/ForwardBoundaryCheck.php
@@ -5,7 +5,14 @@ require_once("SimpleBoundaryChecker.php");
 
 class ForwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryChecker {
   public function check() {
-
+    foreach($this->boundaries as $boundary) {
+      $pos_new = strpos($this->m_data, $boundary, $this->m_pos);
+      if($pos_new!==false) {
+        $this->m_pos = $pos_new;
+      } else {
+        throw new Exception("unrecognized page format");
+      }
+    }
   }
 }
 
