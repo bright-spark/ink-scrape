c9fe50dc051b8ec22d0de4b91ce5f930de02488b
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 14:49:19 +0000

    return instance, so user get more info

--- trunk/ink-scrape.php
+++ trunk/ink-scrape.php
@@ -7,11 +7,13 @@ class InkScrape {
   public static function checkFrontBoundariesForText($boundaries, $text, $pos=null) {
     $fbc = new ForwardBoundaryCheck($boundaries, $text, $pos);
     $fbc->check();
+    return $fbc;
   }
 
   public static function checkBackBoundariesForText($boundaries, $text, $pos=null) {
     $bbc = new BackwardBoundaryCheck($boundaries, $text, $pos);
     $bbc->check();
+    return $bbc;
   }
 }
 
