91d6373c669e28901c77cc95ab568a70d3f6c8f8
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 12:50:25 +0000

    add accessors and save data

--- branches/0.5/base/UnmatchedBoundaryException.php
+++ branches/0.5/base/UnmatchedBoundaryException.php
@@ -1,7 +1,27 @@
 <?php
 
 class UnmatchedBoundaryException extends Exception {
+  private $m_position;
+  private $m_boundary;
+  private $m_text;
+
+  public function position() {
+    return $this->m_position;
+  }
+
+  public function boundary() {
+    return $this->m_boundary;
+  }
+
+  public function text() {
+    return $this->m_text;
+  }
+
   public function __construct($position, $boundary, $text) {
+    $this->m_position = $position;
+    $this->m_boundary = $boundary;
+    $this->m_text = $text;
+
     $str = substr($text, $position, 15);
     parent::__construct("boundary not found in text at or after position {$position}:\n\tboundary: {$boundary}\n\ttext (after {$position}): '{$str}'\n");
   }
