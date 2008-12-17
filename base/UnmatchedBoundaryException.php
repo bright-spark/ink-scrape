a453a48671dae0dbe5e854af81a982302b0a2ec5
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 09:35:37 +0000

    add position info

--- base/UnmatchedBoundaryException.php
+++ base/UnmatchedBoundaryException.php
@@ -1,8 +1,8 @@
 <?php
 
 class UnmatchedBoundaryException extends Exception {
-  public function __construct($boundary, $text) {
-    parent::__construct("boundary not found in text:\n\tboundary: {$boundary}\n\ttext: {$text}\n");
+  public function __construct($position, $boundary, $text) {
+    parent::__construct("boundary not found in text at or after position {$position}:\n\tboundary: {$boundary}\n\ttext: {$text}\n");
   }
 }
 
