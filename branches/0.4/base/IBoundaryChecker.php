8a7befb8f64c10e44b767a1573917317d758df3f
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-17 08:12:31 +0000

    add access to 'data' (data to be matched against)

--- branches/0.4/base/IBoundaryChecker.php
+++ branches/0.4/base/IBoundaryChecker.php
@@ -4,6 +4,9 @@ interface IBoundaryChecker {
   public function boundaries();
   public function setBoundaries($boundaries);
 
+  public function data();
+  public function setData($data);
+
   public function currentPosition();
   public function setPosition($position);
 
--- branches/0.4/base/SimpleBoundaryChecker.php
+++ branches/0.4/base/SimpleBoundaryChecker.php
@@ -2,6 +2,7 @@
 
 class SimpleBoundaryChecker {
   protected $m_boundaries;
+  protected $m_data;
   protected $m_pos;
 
   public function boundaries() {
@@ -12,12 +13,20 @@ class SimpleBoundaryChecker {
     $this->boundaries = $boundaries;
   }
 
+  public function data() {
+    return $this->m_data;
+  }
+
+  public function setData($data) {
+    $this->m_data = $data;
+  }
+
   public function currentPosition() {
     return $this->m_pos;
   }
 
   public function setPosition($position) {
-    $this->pos = $position
+    $this->pos = $position;
   }
 }
 
