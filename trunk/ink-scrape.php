929927106c447c2d4234e5457014fa8235e2f03a
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 01:38:03 +0000

    make check method return boolean value

--- trunk/ink-scrape.php
+++ trunk/ink-scrape.php
@@ -4,22 +4,31 @@ include("base/ForwardBoundaryCheck.php");
 
 class InkScrape {
   public static function checkFrontBoundariesForText($boundaries, $text, $pos=null) {
-    $fbc = new ForwardBoundaryCheck($boundaries, $text, $pos);
-    $fbc->check();
-    return $fbc;
+    $success = true;
+    try {
+      $fbc = new ForwardBoundaryCheck($boundaries, $text, $pos);
+      $fbc->check();
+    } catch(UnmatchedBoundaryException $e) {
+      $success = false;
+    }
+    return $success;
   }
 
   public static function textWithFrontBoundaries($boundaries, $text, $pos=null) {
-    $fbc = self::checkFrontBoundariesForText($boundaries, $text, $pos);
+    $fbc = new ForwardBoundaryCheck($boundaries, $text, $pos);
+    $fbc->check();
     $str = substr($text, $fbc->currentPosition());
     return $str;
   }
 
   public static function boundedText($front, $back, $text, $pos=null) {
-    $fbc1 = self::checkFrontBoundariesForText($front, $text, $pos);
+    $fbc1 = new ForwardBoundaryCheck($front, $text, $pos);
+    $fbc1->check();
     $pos2 = $fbc1->currentPosition();
     $text2 = substr($text, $pos2);
-    $fbc2 = self::checkFrontBoundariesForText($back, $text2);
+
+    $fbc2 = new ForwardBoundaryCheck($back, $text2, null);
+    $fbc2->check();
     $pos3 = strpos($text2, $back[0]);
     //if i'm still here, that means boundaries are there
     return substr($text, $pos2, $pos3);
