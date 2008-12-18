26c89111102142068aac804a56424c2b13214545
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 12:18:56 +0000

    basic implementation

--- branches/dev/parsing/Tag.php
+++ branches/dev/parsing/Tag.php
@@ -1,7 +1,27 @@
 <?php
 
 class Tag {
+  protected $m_name;
+  protected $m_attributes;
+  protected $m_body;
 
+  public function __construct($name="", $attributes=array(), $body="") {
+    $this->m_name = $name;
+    $this->m_attributes = $attributes;
+    $this->m_body = $body;
+  }
+
+  public function name() {
+    return $this->m_name;
+  }
+
+  public function attributes() {
+    return $this->m_attributes;
+  }
+
+  public function body() {
+    return $this->m_body;
+  }
 }
 
 ?>
