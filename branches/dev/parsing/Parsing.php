0da15743fba050264bb98df2f6e9edcd732832c6
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 12:20:53 +0000

    add exceptions

--- branches/dev/parsing/Parsing.php
+++ branches/dev/parsing/Parsing.php
@@ -2,15 +2,18 @@
 
 require_once("Tag.php");
 
+class NoAttributeFoundException extends Exception {}
+class NoTagNameFoundException extends Exception {}
+
 class Parsing {
   public static function parseUnclosedTag($str) {
     $ret = preg_match('/<([^\s]+)(.+)>/', $str, $matches);
-    if($ret===false || $ret<=0) throw new UnexpectedValueException("unable to find <input> elements");
+    if($ret===false || $ret<=0) throw new NoTagNameFoundException("unable to find <input> elements");
     $tag_name = $matches[1];
 
     try {
       $tag_attributes = self::parseAttributes($matches[2]);
-    } catch (UnexpectedValueException $e) {
+    } catch (NoAttributeFoundException $e) {
       //no attributes found
       $tag_attributes = array();
     }
@@ -21,7 +24,7 @@ class Parsing {
 
   public static function parseAttributes($attr_str) {
     $ret = preg_match_all('/\s*([^=]+)="([^"]+)"/', $attr_str, $matches);
-    if($ret===false || $ret<=0) throw new UnexpectedValueException("unable to find attributes");
+    if($ret===false || $ret<=0) throw new NoAttributeFoundException("unable to find attributes");
 
     $attrs = array_combine($matches[1], $matches[2]);
     return $attrs;
