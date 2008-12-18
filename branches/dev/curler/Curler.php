c6bb5aa0747d2829b005027611ef7227206d67f8
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 03:59:56 +0000

    add basic methods

--- branches/dev/curler/Curler.php
+++ branches/dev/curler/Curler.php
@@ -1,7 +1,50 @@
 <?php
 
 class Curler {
+  public $options;
 
+  protected $m_previousUrl;
+  protected $m_currentUrl;
+
+  public function __construct($options=array()) {
+    $this->options = $options;
+    $this->m_previousUrl = '';
+    $this->m_currentUrl = '';
+  }
+
+  public function sendGetToUrl($url, $options=array()) {
+    $options[CURLOPT_HTTPGET] = true;
+    $this->sendRequestToUrl($url, $options);
+  }
+
+  public function sendPostToUrl($url, $options=array()) {
+    $options[CURLOPT_POST] = true;
+    $this->sendRequestToUrl($url, $options);
+  }
+
+  protected function validateParameters(&$options) {
+    $options[CURLOPT_RETURNTRANSFER] = true;
+  }
+
+  protected function sendRequestToUrl($url, $options=null) {
+    $opt_arr = array_merge($this->m_options, $options);
+    $this->validateParameters(&$options);
+
+    $ch = curl_init();
+    if(function_exists('curl_setopt_array')) {
+      curl_setopt_array($ch, $options);
+    } else {
+      foreach($options as $opt=>$val) {
+        curl_setopt($ch, $opt, $val);
+      }
+    }
+
+    $returnValue = curl_exec($ch);
+    if(empty($returnValue)) {
+      curl_error($ch);
+    }
+    curl_close();
+  }
 }
 
 ?>
