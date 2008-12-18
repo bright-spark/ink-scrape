8d24df54f19c1aeb8c1e2af44e36d9c1d5c1abd0
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 12:19:41 +0000

    add parsing of unclosed tags (eg. <tag>)

--- branches/dev/parsing/Parsing.php
+++ branches/dev/parsing/Parsing.php
@@ -3,6 +3,22 @@
 require_once("Tag.php");
 
 class Parsing {
+  public static function parseUnclosedTag($str) {
+    $ret = preg_match('/<([^\s]+)(.+)>/', $str, $matches);
+    if($ret===false || $ret<=0) throw new UnexpectedValueException("unable to find <input> elements");
+    $tag_name = $matches[1];
+
+    try {
+      $tag_attributes = self::parseAttributes($matches[2]);
+    } catch (UnexpectedValueException $e) {
+      //no attributes found
+      $tag_attributes = array();
+    }
+
+    $tag = new Tag($tag_name, $tag_attributes);
+    return $tag;
+  }
+
   public static function parseAttributes($attr_str) {
     $ret = preg_match_all('/\s*([^=]+)="([^"]+)"/', $attr_str, $matches);
     if($ret===false || $ret<=0) throw new UnexpectedValueException("unable to find attributes");
