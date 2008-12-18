879c986d38e83c535e368feace813723402f0bd1
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 08:06:51 +0000

    rename variable

--- branches/dev/bounds/ForwardBoundaryCheck.php
+++ branches/dev/bounds/ForwardBoundaryCheck.php
@@ -15,18 +15,18 @@ class ForwardBoundaryCheck extends SimpleBoundaryChecker implements IBoundaryChe
     $count = 0;
     $this->validateParameters();
     foreach($this->m_boundaries as $boundary) {
-      $pos_new = strpos($this->m_data, $boundary, $this->m_pos);
-      if($pos_new!==false) {
+      $boundary_pos = strpos($this->m_data, $boundary, $this->m_pos);
+      if($boundary_pos!==false) {
         if($count===0) {
-          $this->m_firstBoundaryPosition = $pos_new;
+          $this->m_firstBoundaryPosition = $boundary_pos;
         }
-        $this->m_pos = $pos_new+strlen($boundary);
+        $this->m_pos = $boundary_pos+strlen($boundary);
         $count++;
       } else {
         throw new UnmatchedBoundaryException($this->m_pos, $boundary, $this->m_data, $count);
       }
     }
-    $this->m_lastBoundaryPosition = $pos_new;
+    $this->m_lastBoundaryPosition = $boundary_pos;
   }
 }
 
