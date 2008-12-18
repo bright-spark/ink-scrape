f0746e3b8338dc852c2a2d9ad12e6e89ff7b4206
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 08:49:03 +0000

    update tests

--- branches/dev/test.php
+++ branches/dev/test.php
@@ -2,19 +2,20 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-include("ink-scrape.php");
+include("InkScrape.php");
 
-$string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tortor quam, pellentesque at, blandit id, consequat malesuada, lectus. Etiam laoreet quam vel metus. Suspendisse eu risus id justo tincidunt auctor.";
+$inst = new InkScrape();
+$inst->data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tortor quam, pellentesque at, blandit id, consequat malesuada, lectus. Etiam laoreet quam vel metus. Suspendisse eu risus id justo tincidunt auctor.";
 
 /**
  * Test locate-text-general
  * - test ability to locate text (not necessarily in any order)
  */
-$pos = 0;
-InkScrape::checkFrontBoundariesForText(array(
+$inst->position = 0;
+$inst->checkFrontBoundaries(array(
 'Lorem',
 'Vestibulum',
-'Etiam'), $string);
+'Etiam'));
 echo("passed test (locate-text-general)\n");
 
 /**
@@ -22,22 +23,24 @@ echo("passed test (locate-text-general)\n");
  * - test ability to locate text, in specified order
  * - the third string comes before the second string, so this case should fail if position isn't stored
  */
-$return_value = InkScrape::checkFrontBoundariesForText(array(
+$inst->position=0;
+$return_value = $inst->checkFrontBoundaries(array(
   'Lorem',
   'Etiam',
-  'Vestibulum'), $string);
+  'Vestibulum'));
 
-echo((!$return_value ? 'passed' : 'FAILED')." test (locate-text-forward)\n");
+echo(((!$return_value && ($inst->lastBoundaryChecker()->totalBoundariesMatched() == 2)) ? 'passed' : 'FAILED')." test (locate-text-forward)\n");
 
 /**
  * Test locate-text-nonoverlap
  * - test ability to locate text, in specified order, with no overlapping of boundaries
  * - third string starts at the same position as the second string, so this case should fail if position isn't moved beyond previous successful matched boundary
  */
-$return_value = InkScrape::checkFrontBoundariesForText(array(
+$inst->position=0;
+$return_value = $inst->checkFrontBoundaries(array(
   'Lorem',
   'Suspendisse',
-  'Suspend'), $string);
+  'Suspend'));
 
 echo((!$return_value ? 'passed' : 'FAILED')." test (locate-text-nonoverlap)\n");
 
@@ -45,36 +48,11 @@ echo((!$return_value ? 'passed' : 'FAILED')." test (locate-text-nonoverlap)\n");
  * Test locate-bounded-text
  * - test ability to locate text with front and back boundaries
  */
-$pos = 0;
-$matched = false;
-if(
-InkScrape::boundedText(array('Lorem','consectetur','tortor'), array('pellentesque','Etiam','Suspendisse'), $string) == " quam, " &&
-InkScrape::boundedText(array('elit'), array('quam'), $string) == ". Vestibulum tortor "
-) {
-  $matched = true;
-}
+$inst->position=0;
+$matched = true;
+$matched = $matched && ($inst->boundedText(array('Lorem','consectetur','tortor'), array('pellentesque','Etiam','Suspendisse')) == " quam, ");
+$inst->position=0;
+$matched = $matched && ($inst->boundedText(array('elit'), array('quam')) == ". Vestibulum tortor ");
 echo(($matched ? 'passed' : 'FAILED')." test (locate-bounded-text)\n");
-
-/**
- * Test locate-bounded-text-front
- * - test ability to locate text with front boundaries
- */
-$pos = 0;
-$matched = false;
-if(InkScrape::textFollowingFrontBoundaries(array('Lorem','Etiam','Suspendisse','tincidunt'), $string) == " auctor.") {
-  $matched = true;
-}
-echo(($matched ? 'passed' : 'FAILED')." test (locate-bounded-text-front)\n");
-
-/**
- * Test locate-bounded-text-front-arbitrary
- * - test ability to locate text with front boundaries, starting from an arbitrary position
- */
-$pos = strpos($string, "laoreet ")+strlen("laoreet ");
-$matched = false;
-if(InkScrape::textFollowingFrontBoundaries(array('quam'), $string, $pos) == " vel metus. Suspendisse eu risus id justo tincidunt auctor.") {
-  $matched = true;
-}
-echo(($matched ? 'passed' : 'FAILED')." test (locate-bounded-text-front-arbitrary)\n");
 
 ?>
