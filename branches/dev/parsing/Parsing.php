745b1144bcbefd6b72c63cbf0ff292ad2ed0bd44
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-18 12:12:45 +0000

    add attribute parsing

--- branches/dev/parsing/Parsing.php
+++ branches/dev/parsing/Parsing.php
@@ -3,8 +3,12 @@
 require_once("Tag.php");
 
 class Parsing {
-  public static function parseOpeningTag($str) {
+  public static function parseAttributes($attr_str) {
+    $ret = preg_match_all('/\s*([^=]+)="([^"]+)"/', $attr_str, $matches);
+    if($ret===false || $ret<=0) throw new UnexpectedValueException("unable to find attributes");
 
+    $attrs = array_combine($matches[1], $matches[2]);
+    return $attrs;
   }
 }
 
