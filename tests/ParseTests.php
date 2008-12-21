8360b1fa41b3d021a447f13302d535d3a4d1bb1a
rctay89 <rctay89@b985f6f4-cb34-11dd-aa7b-9fdd872f0417>
2008-12-21 07:24:10 +0000

    added test for parsing unclosed named tags

--- tests/ParseTests.php
+++ tests/ParseTests.php
@@ -5,6 +5,17 @@ error_reporting(E_ALL | E_STRICT);
 include("lib/Parsing.php");
 
 $ref_attributes = array("name"=>"John Doe", "age"=>"20");
+$ref_doc = <<<EOD
+<person></person>
+<person/>
+<person    />
+<person name="{$ref_attributes["name"]}"/>
+<person name="{$ref_attributes["name"]}"          />
+
+<persons/>
+<persons       />
+<persons></person>
+EOD;
 
 /**
  * Test parse-attributes
@@ -25,5 +36,27 @@ $tag_attrs = Parsing::parseAttributes("name\n=  \"{$ref_attributes["name"]}\" ag
 $passed = $passed && count($tag_attrs)==2 && array_key_exists("name", $tag_attrs) && array_key_exists("age", $tag_attrs) && ($tag_attrs["name"] == $ref_attributes["name"]) && ($tag_attrs["age"] == $ref_attributes["age"]);
 
 echo(($passed ? 'passed' : 'failed')." test (parse-attributes)\n");
+
+/**
+ * Test parse-unclosed-named-tags
+ */
+$passed = true;
+
+$tags = Parsing::parseAllUnclosedTags($ref_doc, "person");
+$passed = $passed && count($tags)==5;
+
+//start at index 0, for 3 items
+for($i=0;$i<3;$i++) {
+  $tag_attrs = $tags[$i]->attributes();
+  $passed = $passed && array_key_exists($i, $tags) && empty($tag_attrs);
+}
+
+//start at index 3, for 2 items
+for($i=3;$i<2+3;$i++) {
+  $tag_attrs = $tags[$i]->attributes();
+  $passed = $passed && array_key_exists($i, $tags) && !empty($tag_attrs) && array_key_exists("name", $tag_attrs) && ($tag_attrs["name"] == $ref_attributes["name"]);
+}
+
+echo(($passed ? 'passed' : 'failed')." test (parse-unclosed-named-tags)\n");
 
 ?>
